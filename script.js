65cdda5f834a32fcdf9d9db2c9b538db6bd29671
paulosergio <paulosergiobarrosdesouza@gmail.com>
2024-07-10 21:13:08 -0300

    Faze 1

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("teste")
+let nomeEsperiencia = [
+  ["Caião", 1500]
+]
+
+console.log("Olá aventureiro, pronto para uma nova aventura?")
+console.log("Para começarmos, me dê o seu nome e qual o seu atual XP.")
+
