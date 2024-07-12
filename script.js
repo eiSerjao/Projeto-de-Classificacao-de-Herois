7817119ce5e22c00ce88c01dfe5c6d661f8e1063
paulosergio <paulosergiobarrosdesouza@gmail.com>
2024-07-12 15:39:45 -0300

    Fase 4

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let nome = "Caião"
-let nivelXP = 10500
-let patente = ""
+let aventureiro = [
+  ["Caião", 1001]
+];
 
 console.log("Olá aventureiro, pronto para uma nova aventura?")
 console.log("Para começarmos, me dê o seu nome e qual o seu atual XP.")
 
-if (nivelXP < 1000){
+if (aventureiro[0][1] < 1000){
   patente = "Ferro";
-} else if (nivelXP >= 1001 && nivelXP <= 2000){
+} else if (aventureiro[0][1] >= 1001 && aventureiro[0][1] <= 2000){
   patente = "Bronze"
-} else if (nivelXP >= 2001 && nivelXP <= 5000){
+} else if (aventureiro[0][1] >= 2001 && aventureiro[0][1] <= 5000){
   patente = "Prata"
-} else if (nivelXP >= 5001 && nivelXP <= 7000){
+} else if (aventureiro[0][1] >= 5001 && aventureiro[0][1] <= 7000){
   patente = "Ouro"
-} else if (nivelXP >= 7001 && nivelXP <= 8000){
+} else if (aventureiro[0][1] >= 7001 && aventureiro[0][1] <= 8000){
   patente = "PLatina"
-} else if (nivelXP >= 8001 && nivelXP <= 9000){
+} else if (aventureiro[0][1] >= 8001 && aventureiro[0][1] <= 9000){
   patente = "Ascendente"
-} else if (nivelXP >= 9001 && nivelXP <= 10000){
+} else if (aventureiro[0][1] >= 9001 && aventureiro[0][1] <= 10000){
   patente = "Ascendente"
-} else if (nivelXP >= 10001){
+} else if (aventureiro[0][1] >= 10001){
   patente = "Radiante"
 } else {
-  ("Numero Invalido de Xp")
-}
+  patente = "Radiante"
+} 
 
 console.log("Sua patente é do nivel " + patente)
